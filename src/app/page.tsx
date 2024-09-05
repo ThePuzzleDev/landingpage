b760db09d22a9c2178c807012580e87feb73e968
Salman Mhaskar <salmanmhaskar17@gmail.com>
2024-09-05 13:40:58 +0530

    herosection added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 
 import Services from '@/components/Services';
 import PuzzleScene from '../components/PuzzleScene';
+import HeroSection from '@/components/HeroSection';
+import ContactUs from '@/components/ContactUs';
+import BottomBar from '@/components/BottomBar';
 
 export default function Home() {
   return (
     <div>
-      <PuzzleScene/>
+      <HeroSection id="hero-section"/>
       <Services/>
+      <ContactUs/>
     </div>
   );
 }
