97405dfc4d6c06682dbf1ea579543c5549d191bc
aaseem22 <aaseem.mhaskar.am@gmail.com>
2024-09-06 21:26:26 +0530

    Description issue fix

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -6,13 +6,13 @@ interface DescriptionProps {
   text: string; // Optional icon prop (app icon)
   imageSrc: string | StaticImageData;  // Image to display inside the card
 }
+
 const Description: React.FC<DescriptionProps> = ({ text, imageSrc }) => {
   return (
-    <div className="flex justify-center items-center h-72 bg-black p-4">
-    
-        <div className="flex items-center space-x-6">
-        <div className="w-90 bg-white rounded-2xl p-6 shadow-lg">
-          <div className="flex-shrink-0">
+    <div className="flex justify-center items-center h-full bg-black p-4">
+      <div className="flex flex-col items-center space-y-6">
+        <div className="w-full max-w-sm sm:max-w-sm md:max-w-lg bg-white rounded-2xl p-6 shadow-lg">
+          <div className="flex-shrink-0 w-full sm:w-1/2 md:w-auto">
             <ImageComponent
               src={imageSrc}
               alt="App Icon"
@@ -21,18 +21,17 @@ const Description: React.FC<DescriptionProps> = ({ text, imageSrc }) => {
               className="rounded-lg overflow-hidden"
             />
           </div>
-          </div>
-          <div className="flex-grow">
-            <div className="bg-gradient-to-r from-[#1C1B1B] via-[#464545] to-[#939393] rounded-full py-4 px-6">
-            <span className="typewriter  text-white  px-2 ">
-              <p className="text-white text-base">{text}</p>
-              </span>
-            </div>
-
+        </div>
+        <div className="flex-grow">
+          <div className="bg-gradient-to-r from-[#1C1B1B] via-[#464545] to-[#939393] rounded-2xl py-4 px-6">
+            <span className=" text-white px-2">
+              <p className="text-white text-sm">{text}</p>
+            </span>
           </div>
         </div>
       </div>
-    
+    </div>
   );
 };
+
 export default Description;
