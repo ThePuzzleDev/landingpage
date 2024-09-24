463acc5559a7b80646de5ac70f1b4a2f9d630459
Salman Mhaskar <salmanmhaskar17@gmail.com>
2024-09-24 23:22:12 +0530

    added ingregenius download link

--- src/components/Download.js
+++ src/components/Download.js
@@ -1,8 +1,8 @@
 export default function DownloadButton() {
     return (
       <div className="my-6 text-center">
-        <a href="#" className="bg-white text-black px-6 py-2 rounded-full font-bold hover:bg-gray-200 transition-colors">
-          Coming Soon
+        <a href="https://apps.apple.com/in/app/ingregenius/id6670257708" className="bg-white text-black px-6 py-2 rounded-full font-bold hover:bg-gray-200 transition-colors">
+          Download on AppStore
         </a>
       </div>
     );
